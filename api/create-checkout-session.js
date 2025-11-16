d7a64ae457e0180249cbc815426dff23e4f458ed
Khethelo89 <bonginhlanhladlamini434@gmail.com>
2025-11-16 23:23:06 +0200

    Create create-checkout-session.js

---
+++ api/create-checkout-session.js
@@ -0,0 +1,10 @@
+export default async function handler (req,res){
+  if(req.method !== "POST"){
+    return res.status(405).json({error: "Method Not Allowed"});
+  }
+
+// for now, simple test response
+res.status(200).json({
+  message: "Create checkout session working!"
+});
+}
