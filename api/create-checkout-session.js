1f6b0178625aea2ef3bb3b2e667e5912562e9fb2
Khethelo89 <bonginhlanhladlamini434@gmail.com>
2025-11-17 02:15:24 +0200

    Update create-checkout-session.js

--- api/create-checkout-session.js
+++ api/create-checkout-session.js
@@ -24,7 +24,7 @@ export default async function handler(req, res) {
     const formattedLineItems = lineItems.map(item => ({
       displayName: item.name,
       quantity: item.quantity,
-      pricingDetails: { amountInCents: item.amount}
+      pricingDetails: { price : item.amount}
     }));
 
     // Call Yoco Checkout API
